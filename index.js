83d892992fa28d4ca8961d64f54817b9d6126aba
wesley-hall <hallwesley@outlook.com>
2019-03-22 12:02:30 +0000

    index.js and environment.js updated

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
 const { dbURI, port } = require('./config/environment')
+const app = express()
 
 mongoose.connect(dbURI, { useNewUrlParser: true })
 
-const app = express()
+app.use(express.static(`${__dirname}/dist`))
 
 app.use(bodyParser.json())
 
