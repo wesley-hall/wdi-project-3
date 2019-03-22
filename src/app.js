4167af7a21da9814ccef99a9a6b7168fffa56133
orjon <github@orjon.com>
2019-03-22 17:38:11 +0000

    Books All getting books data

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ class App extends React.Component {
 
   render() {
     return(
+
       <div>
         <h1>Hello World!</h1>
         <Home />
--- src/components/pages/booksAll.js
+++ src/components/pages/booksAll.js
@@ -23,28 +23,28 @@ class BooksAll extends React.Component {
             {!this.state.books && <p>...loading</p>}
             {this.state.books && this.state.books.map(book => (
               <div key={book._id} className="column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
-                <Link to={`/books/${book._id}`} >
-                  <div className="card">
-                    <div className="card-header">
-                      <h4 className="card-header-title">{book.title}</h4>
-                      <h5 className="title is-6">{book.authors[0]}</h5>
-                    </div>
-                    <div className="card-image">
-                      <figure className="image">
-                        <img src={book.image} alt={book.title} />
-                      </figure>
-                    </div>
-                    <div className="card-content">
-                      <h5 className="title is-6">{book.genre}</h5>
-                      <h5 className="title is-6">{book.fiction}</h5>
-                      <h5 className="title is-6">{book.description}</h5>
-                      <h5 className="title is-6">{book.rating}</h5>
-                      <h5 className="title is-6">{book.returnDate}</h5>
-                      <h6 className="subtitle is-6">{book.owner.libraryName}</h6>
-                      <h6 className="subtitle is-6">{book.owner.location}</h6>
-                    </div>
+
+                <div className="card">
+                  <div className="card-header">
+                    <h4 className="card-header-title">Title: {book.title}</h4>
+                    <h5 className="title is-6">Author(s): {book.authors[0]}</h5>
                   </div>
-                </Link>
+                  <div className="card-image">
+                    <figure className="image">
+                      <img src={book.image} alt={book.title} />
+                    </figure>
+                  </div>
+                  <div className="card-content">
+                    <h5 className="title is-6">Genre: {book.genre}</h5>
+                    <h5 className="title is-6">Fiction: {book.fiction}</h5>
+                    <h5 className="title is-6">Description: {book.description}</h5>
+                    <h5 className="title is-6">Rating: {book.rating[0].rating}</h5>
+                    <h5 className="title is-6">Review: {book.review[0].review}</h5>
+                    <h5 className="title is-6">Return Date: {book.returnDate}</h5>
+                    <h6 className="subtitle is-6">Owner: {book.owner}</h6>
+                  </div>
+                </div>
+
               </div>
             ))}
           </div>
@@ -54,3 +54,7 @@ class BooksAll extends React.Component {
   }
 }
 export default BooksAll
+
+
+// <Link to={`/books/${book._id}`} >
+// </Link>
