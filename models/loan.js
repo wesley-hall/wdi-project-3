f86c76441339dbc821335230e3353b16e88b0211
Your Name <sumisastri@gmail.com>
2019-03-23 16:25:52 +0000

    loanSchema1

---
+++ models/loan.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const loanSchema = new mongoose.Schema({
+  book: { type: mongoose.Schema.ObjectId, ref: 'Book'},
+  borrower: { type: mongoose.Schema.ObjectId, ref: 'User'},
+  start: { type: Date, required: true},
+  end: { type: Date, required: true},
+  message: { type: String },
+  returned: { type: Date},
+  approved: { type: Boolean, required: true }
+}, {
+  timestamps: true
+})
+
+
+loanSchema.plugin(require('mongoose-unique-validator'))
+
+module.exports = mongoose.model('Loan', loanSchema)
