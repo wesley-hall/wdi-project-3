000d3cb5d7ad062727c7997ccf37761e072ec893
orjon <github@orjon.com>
2019-03-23 11:33:39 +0000

    Basic routing set up

---
+++ src/components/books/bookShow.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class BookShow extends React.Component {
+  constructor() {
+    super()
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Book Show</h1>
+      </div>
+    )
+  }
+}
+
+export default BookShow
---
+++ src/components/books/bookUpdate.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class BookUpdate extends React.Component {
+  constructor() {
+    super()
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Book Update</h1>
+      </div>
+    )
+  }
+}
+
+export default BookUpdate
