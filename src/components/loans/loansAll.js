e83c676ca03f92643d20646f2dcda28c84baa51b
Your Name <sumisastri@gmail.com>
2019-03-24 16:53:37 +0000

    loansAll updated

--- src/components/loans/loansAll.js
+++ src/components/loans/loansAll.js
@@ -5,24 +5,78 @@ import { Link } from 'react-router-dom'
 class LoansAll extends React.Component {
   constructor() {
     super()
-
     this.state = {}
-
   }
 
   componentDidMount() {
-    // axios.get('/api/loans')
-    //   .then(res => this.setState({ loans: res.data }))
+    axios.get('/api/loans')
+      .then(res => this.setState({ loans: res.data }))
   }
 
   render() {
     console.log(this.state.loans)
+    const { loans } = this.state
     return (
-      <main className="section">
-        <div className="container">
-          <h1>loansAll</h1>
-        </div>
-      </main>
+      <div>
+        <main className="section">
+          <div className="container">
+
+            <h1>My account</h1>
+
+            <div>
+              <h2>Books Loaned Out</h2>
+              {loans.map(loan => (
+                <div key={loan._id}>
+                  <h4>{loan.start}</h4>
+                  <h4>{loan.end}</h4>
+                  <h4>{loan.book}</h4>
+                  <h4>{loan.borrower}</h4>
+                  <h4>{loan.returned}</h4>
+                  <h4>{loan.approved}</h4>
+
+                  <p>Select:</p>
+                  <select
+                    name="booksLoaned"
+                    defaultValue={2}
+                    onChange={this.handleChange}
+                  >
+                    <option value="1">On Loan</option>
+                    <option value="2">Pending Approval</option>
+                    <option value="2">Returned</option>
+                    <option value="3">Overdue</option>
+                  </select>
+                </div>
+              ))}
+            </div>
+
+            <div>
+              <h2>Books Borrowed</h2>
+              {loans.map(loan => (
+                <div key={loan._id}>
+                  <h4>{loan.start}</h4>
+                  <h4>{loan.end}</h4>
+                  <h4>{loan.book}</h4>
+                  <h4>{loan.borrower}</h4>
+                  <h4>{loan.returned}</h4>
+
+                  <p>Select:</p>
+                  <select
+                    name="booksBorrowed"
+                    defaultValue={2}
+                    onChange={this.handleChange}
+                  >
+                    <option value="1">Books Borrowed</option>
+                    <option value="2">Waiting for Confirmation</option>
+                    <option value="2">Returned</option>
+                    <option value="3">Overdue</option>
+                  </select>
+                </div>
+              ))}
+            </div>
+
+          </div>
+        </main>
+      </div>
     )
   }
 }
