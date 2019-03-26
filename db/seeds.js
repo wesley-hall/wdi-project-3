7bcc5cea7c6fa342d866f2d7d88ac55c6906dd47
orjon <github@orjon.com>
2019-03-26 23:12:16 +0000

    seeding1

--- db/seeds.js
+++ db/seeds.js
@@ -391,6 +391,119 @@ mongoose.connect(dbURI, { useNewUrlParser: true }, (err, db) => {
                 user: users[3]
               }
             ]
+          },
+          ///////////////////////////////
+          {
+            owner: users[0],
+            title: 'The Study of Plant Life',
+            authors: 'M. C. Stopes',
+            image: 'http://www.orjon.com/dev/booker/images/bookcovers/85.jpeg',
+            fiction: true,
+            genre: genres[2],
+            description: 'Facilisi nullam vehicula ipsum a arcu cursus vitae. Turpis nunc eget lorem dolor sed viverra ipsum. Iaculis nunc sed augue lacus viverra. Orci sagittis eu volutpat odio facilisis mauris. Morbi tristique senectus et netus. Augue eget arcu dictum varius duis at consectetur lorem. Praesent elementum facilisis leo vel. Imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada.',
+            rating: [
+              { rating: 4, user: users[2]}
+            ],
+            review: [
+              {
+                review: 'Nibh nostrum ne duo, nullam splendide quo ea, vix eros quidam forensibus id. Et vim reque abhorreant, mel ex reque munere, cu eirmod vocent deseruisse mel. Ei dolorem detraxit pri. Ea eum semper cotidieque disputando, nam albucius nominati accommodare ne. Est at quod ancillae reformidans, ius id falli equidem moderatius. Est eu partem quaeque. No unum sale vel, nec ea blandit percipit expetenda.',
+                user: users[5]
+              },
+              {
+                review: 'Commodo posidonium pro ne. Has malis mucius everti ad, ad eum tacimates vituperata, alia modus vel et. Vim quot sonet cu, voluptua posidonium vis in. Ex option euismod theophrastus nec, sea ad discere luptatum. Pri quodsi integre reformidans ea, nibh tota mel ex. Cu facete propriae duo, his tollit iisque constituam ei, cu vel quot dolores deseruisse.',
+                user: users[6]
+              },
+              {
+                review: 'Facilisi nullam vehicula ipsum a arcu cursus vitae. Turpis nunc eget lorem dolor sed viverra ipsum. Iaculis nunc sed augue lacus viverra. Orci sagittis eu volutpat odio facilisis mauris. Morbi tristique senectus et netus. Augue eget arcu dictum varius duis at consectetur lorem. Praesent elementum facilisis leo vel. Imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada.',
+                user: users[1]
+              },
+              {
+                review: 'Cu evertitur consequat sit, tale tempor blandit an mea. Ex eripuit deleniti gubergren quo, in sed illum minimum.',
+                user: users[4]
+              }
+            ]
+          },
+          {
+            owner: users[0],
+            title: 'Faulkner\'s Folly',
+            authors: 'Carolyn Wells',
+            image: 'http://www.orjon.com/dev/booker/images/bookcovers/86.jpeg',
+            fiction: true,
+            genre: genres[2],
+            description: 'Choro deterruisset cu usu, postea nostro percipit eum an. Te mei platonem recteque, eam ut causae discere veritus, cum in labitur eligendi lobortis. Mel mentitum disputationi an, eos ut quaerendum liberavisse ullamcorper. Ea graeci delectus mea. Has deserunt expetenda evertitur at. An eos essent euripidis intellegam, novum albucius oporteat sed ut.',
+            rating: [
+              { rating: 4, user: users[2]}
+            ],
+            review: [
+              {
+                review: 'Odio utamur scripserit sit eu, eam ne salutandi molestiae temporibus. Mea id sapientem disputationi mediocritatem. Ei pro diam erant. Dicit nobis cu cum. Vel movet labitur adversarium in.',
+                user: users[8]
+              },
+              {
+                review: 'Et mei laudem virtute fuisset. At mea eros zril. Cu hinc dolores sit, in pro inani iudicabit, luptatum erroribus has eu. In numquam detracto appetere eum, duo fugit utroque theophrastus ne.\nTe vix mucius nostro. Per perfecto quaerendum ei, summo oblique accusam cu vim. Et duo magna autem suscipiantur. Vis wisi vituperatoribus ex, mei duis ferri temporibus eu. Ut eos erant accusam, veritus maiorum omittam ea ius.',
+                user: users[9]
+              },
+              {
+                review: 'Commodo posidonium pro ne. Vix ut noluisse verterem, paulo sensibus vix ex. Melius oporteat tractatos mei ut, sed ex quod propriae, patrioque consequat intellegebat quo id. Sea eius magna erant ut, ex novum aeque eum, aliquando conceptam maiestatis pro et. Veniam detracto ad eum, nusquam minimum ad mel, te assum vulputate inciderint qui. Has malis mucius everti ad, ad eum tacimates vituperata, alia modus vel et. Vim quot sonet cu, voluptua posidonium vis in.\nHas natum utinam voluptatibus at, et ius homero abhorreant rationibus. Te est tamquam scaevola, nihil mnesarchum te per. Te vim senserit reprimique percipitur. Eum lobortis forensibus deseruisse no, ignota singulis ex eum, augue offendit et cum.\nPro malis paulo doctus ei, nec voluptua expetendis cu. Ex option euismod theophrastus nec, sea ad discere luptatum. Pri quodsi integre reformidans ea, nibh tota mel ex. Cu facete propriae duo, his tollit iisque constituam ei, cu vel quot dolores deseruisse.',
+                user: users[3]
+              },
+            ]
+          },
+          {
+            owner: users[0],
+            title: 'Persuasion',
+            authors: 'Jane Austen',
+            image: 'http://www.orjon.com/dev/booker/images/bookcovers/87.jpeg',
+            fiction: true,
+            genre: genres[2],
+            description: 'Lorem ipsum dolor sit amet, accusam corpora ad duo, vis in eius forensibus, vix ut hinc voluptua. Eu pri melius accusamus percipitur, ad mea mucius neglegentur. Nec eu integre qualisque. Dico soluta perfecto nec id, tacimates definitiones nec ex, vel option accusata quaerendum ex. Cum et cibo ubique. Natum accusamus inciderint eos eu, mea te vero viderer.\nWisi blandit democritum ex his. Vim ancillae scripserit cotidieque ne. Has cu velit feugait rationibus, eu vero epicuri cotidieque eam. No accusata explicari repudiandae usu, ei meis fuisset est. Et iudico soluta vis. An zril invidunt duo.\nChoro deterruisset cu usu, postea nostro percipit eum an. Te mei platonem recteque, eam ut causae discere veritus, cum in labitur eligendi lobortis. Mel mentitum disputationi an, eos ut quaerendum liberavisse ullamcorper. Ea graeci delectus mea. Has deserunt expetenda evertitur at. An eos essent euripidis intellegam, novum albucius oporteat sed ut.',
+            rating: [
+              { rating: 4, user: users[2]}
+            ],
+            review: [
+              {
+                review: 'An vix dictas deseruisse reformidans. An vel corpora facilisi. Vitae mollis euripidis ex cum, sed in primis commodo reformidans. Vidit dignissim efficiantur est et, agam oratio voluptatum no has. Eum nibh mollis ad, sed vitae essent eu.\nSit purto verear iracundia ea, cu mea expetenda gubergren. Te quis partiendo vix. Vivendo voluptatum cu vis, unum minim pertinacia vis ea. Dictas animal suavitate sed ea. No eam dicat erroribus disputando, graecis luptatum partiendo cu ius. Quodsi malorum omittantur sit an, id est mucius temporibus.\nCum eu illum simul, ad velit aliquam has. Dolorum officiis eos et, id usu mentitum sadipscing, veri admodum copiosae ne eum. Invenire definiebas te sea, consul timeam appellantur his cu. Nec lorem quando volutpat cu, et quodsi aperiri eum. Eu consul fabellas gubergren per, ut augue iusto simul mei. Quot omnium quo eu, cu modus illum mea.',
+                user: users[1]
+              },
+              {
+                review: 'Sit purto verear iracundia ea, cu mea expetenda gubergren. Te quis partiendo vix. Vivendo voluptatum cu vis, unum minim pertinacia vis ea. Dictas animal suavitate sed ea. No eam dicat erroribus disputando, graecis luptatum partiendo cu ius. Quodsi malorum omittantur sit an, id est mucius temporibus.\nCum eu illum simul, ad velit aliquam has. Dolorum officiis eos et, id usu mentitum sadipscing, veri admodum copiosae ne eum. Invenire definiebas te sea, consul timeam appellantur his cu. Nec lorem quando volutpat cu, et quodsi aperiri eum. Eu consul fabellas gubergren per, ut augue iusto simul mei. Quot omnium quo eu, cu modus illum mea.',
+                user: users[2]
+              },
+              {
+                review: 'Te his adhuc vituperata, et mei bonorum facilis epicuri. Vis dico erant dolor ea. Eum an suas inani efficiendi, mei ne congue salutandi. Et ipsum laboramus vim, cu eos dicunt aliquando liberavisse.',
+                user: users[2]
+              },
+              {
+                review: 'At vel amet vivendum. Brute ignota nemore sit id, vis ne quaeque accumsan. Malis pertinacia omittantur ex vim, vis ne quidam invenire adversarium, quaerendum interpretaris ne qui. Cum ex menandri sensibus iudicabit, te suscipit perfecto pri.\nCu facete propriae duo, his tollit iisque constituam ei, cu vel quot dolores deseruisse.',
+                user: users[0]
+              },
+            ]
+          },
+          {
+            owner: users[0],
+            title: 'The Jungle',
+            authors: 'Upton Sinclair',
+            image: 'http://www.orjon.com/dev/booker/images/bookcovers/88.jpeg',
+            fiction: true,
+            genre: genres[2],
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            rating: [
+              { rating: 4, user: users[2]}
+            ],
+            review: [
+              {
+                review: 'Nibh nostrum ne duo, nullam splendide quo ea, vix eros quidam forensibus id. Et vim reque abhorreant, mel ex reque munere, cu eirmod vocent deseruisse mel. Ei dolorem detraxit pri. Ea eum semper cotidieque disputando, nam albucius nominati accommodare ne. Est at quod ancillae reformidans, ius id falli equidem moderatius. Est eu partem quaeque. No unum sale vel, nec ea blandit percipit expetenda.\nEst paulo diceret ea, ut odio audire scribentur ius. Dolor aliquando et per. Vidisse persius gloriatur pro no, appareat quaerendum ei nec. Commodo equidem te qui. Quaeque saperet definitionem ea pri, no vim justo porro atqui.\nNe veniam oportere scriptorem vis, ne has tempor omnium, mel te nominavi interesset. Vix at eirmod fuisset lobortis, ei cum inani aliquid deleniti, duis veniam percipitur has id. Te his adhuc vituperata, et mei bonorum facilis epicuri. Vis dico erant dolor ea. Eum an suas inani efficiendi, mei ne congue salutandi. Et ipsum laboramus vim, cu eos dicunt aliquando liberavisse.',
+                user: users[5]
+              },
+              {
+                review: 'Commodo posidonium pro ne. Has malis mucius everti ad, ad eum tacimates vituperata, alia modus vel et. Vim quot sonet cu, voluptua posidonium vis in.\nHas natum utinam voluptatibus at, et ius homero abhorreant rationibus. Te est tamquam scaevola, nihil mnesarchum te per. Te vim senserit reprimique percipitur. Eum lobortis forensibus deseruisse no, ignota singulis ex eum, augue offendit et cum.\nEx option euismod theophrastus nec, sea ad discere luptatum. Pri quodsi integre reformidans ea, nibh tota mel ex. Cu facete propriae duo, his tollit iisque constituam ei, cu vel quot dolores deseruisse.',
+                user: users[6]
+              },
+              {
+                review: 'Elitr possim vituperatoribus cu ius, placerat complectitur nec et. In iisque invenire intellegam mei, at simul oportere eam. Cu vel inani tollit sententiae, quo ne oblique blandit. Eos quod paulo affert cu, at vel reque ponderum. Ne duo ubique voluptatibus, duo odio liber ancillae in. Eius vocibus sapientem eos at.\nQuem imperdiet eu mea, cum et nominavi gloriatur, delenit dignissim philosophia ut mea. Vim facete dolorum phaedrum id, sint minim quaerendum no per. Usu ea erat falli neglegentur, tota illud eirmod ex pro, ut lucilius instructior vim. Probo eligendi in sit, reformidans adversarium has ex, partiendo dissentiet ne pri. affert utinam epicurei vis. Per te tacimates moderatius dicat.',
+                user: users[7]
+              }
+            ]
           }
         ]),
         users
