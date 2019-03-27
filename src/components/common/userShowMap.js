6d782c267d5c0e0235a04b0317c7d1c7dd4bc06f
wesley-hall <hallwesley@outlook.com>
2019-03-27 10:57:49 +0000

    userShowMap is stripped down version of userEditMap

---
+++ src/components/common/userShowMap.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import mapboxgl from 'mapbox-gl'
+mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN
+
+
+class UserShowMap extends React.Component{
+  constructor() {
+    super()
+  }
+
+  componentDidMount() {
+    this.map = new mapboxgl.Map({
+      container: this.mapDiv,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: this.props.center,
+      zoom: 11
+    })
+
+    this.map.addControl(new mapboxgl.NavigationControl(), 'bottom-right')
+
+    new mapboxgl.Marker()
+      .setLngLat(this.props.center)
+      .addTo(this.map)
+  }
+
+  render() {
+    console.log('userShowMap', this.props.center)
+    return (
+      <div>
+        <div className="map-register" ref={el => this.mapDiv = el} />
+      </div>
+    )
+  }
+}
+
+export default UserShowMap
