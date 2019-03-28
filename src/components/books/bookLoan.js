11d2719edae9d700703803b288125afe6a7f7f09
orjon <github@orjon.com>
2019-03-28 10:43:38 +0000

    moment and date picker removed

--- src/components/books/bookLoan.js
+++ src/components/books/bookLoan.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
-import DatePicker from 'react-datepicker'
 
-import 'react-datepicker/dist/react-datepicker.css'
+
 import Auth from '../../lib/auth'
 
 class BookLoan extends React.Component {
