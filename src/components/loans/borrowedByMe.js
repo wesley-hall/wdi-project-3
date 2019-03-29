551495b874f812541b42b09d5ee424e2758c0511
wesley-hall <hallwesley@outlook.com>
2019-03-29 09:55:26 +0000

    borrowed by me restructured, refactored

--- src/components/loans/borrowedByMe.js
+++ src/components/loans/borrowedByMe.js
@@ -1,17 +1,41 @@
 import React from 'react'
 
+import BorrowedReturned from './statusButtons/borrowedReturned'
 
 class BorrowedByMe extends React.Component {
 
   render() {
-    const { loan, isOnLoan, cancelLoanRequest, isOverdue, isReturned } = this.props
+    const { loan, isPending, isAwaitingCollection, isDeclined, isOnLoan, isOverdue, isReturned, isExpired, redirectToBook, cancelLoanRequest } = this.props
     return (
       <div key={loan._id}>
-        <div className="columns is-mobile">
-          <span className="column is-2 is-gapless">{loan.start.substring(10,-5)}</span>
-          <span className="column is-2 is-gapless">{loan.end.substring(10,-5)}</span>
+        <div className={`columns is-mobile has-text-centered is-vcentered loan-border-bottom ${isOverdue(loan) ? 'has-text-danger has-text-weight-bold' : ''}`}>
+          <span className="column is-3 is-gapless">{loan.start.substring(10,-5)} to {loan.end.substring(10,-5)}</span>
           <span className="column is-2 is-gapless">{loan.book.title}</span>
-          <span className="column is-2 is-gapless"><figure className="image is-64x64"><img className="is-rounded" src={loan.book.owner.profilePicture} /></figure>{loan.book.owner.username}</span>
+
+          <div className="column is-3 is-gapless">
+            <div className="columns is-1 is-marginless is-vcentered">
+              <div className="column is-one-third">
+                <figure className="image is-64x64 is-pulled-right">
+                  <img className="is-rounded" src={loan.book.owner.profilePicture} />
+                </figure>
+              </div>
+              <div className="column is-two-thirds">
+                {loan.book.owner.email}
+              </div>
+            </div>
+          </div>
+
+          {isExpired(loan) &&
+            <div className="column is-4 is-gapless columns">
+              <span className="column is-full is-gapless">Expired</span>
+            </div>
+          }
+          {isDeclined(loan) &&
+            <div className="column is-4 is-gapless columns">
+              <span className="column is-full is-gapless">Declined</span>
+            </div>
+          }
+
 
           {!isOnLoan(loan) && !isOverdue(loan) && !isReturned(loan) &&
             <div className="column is-4 is-gapless columns">
@@ -42,14 +66,10 @@ class BorrowedByMe extends React.Component {
               </div>
           }
           {isReturned(loan) &&
-              <div className="column is-4 is-gapless columns">
-                <span className="column is-half is-gapless">Returned on<br /> {loan.returned && loan.returned.substring(10,-5)}</span>
-                <div className="column is-half is-gapless">
-                  <button className="button is-small is-info" onClick={this.handleClick}>
-                    Rate book?
-                  </button>
-                </div>
-              </div>
+              <BorrowedReturned
+                loan={loan}
+                redirectToBook={redirectToBook}
+              />
           }
 
         </div>
