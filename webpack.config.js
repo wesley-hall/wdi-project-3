a1ec933fb5cd70c131d6a6510a2ce3177f6cc087
wesley-hall <hallwesley@outlook.com>
2019-06-28 11:16:28 +0100

    dotenv-webpack added back for local dev for mapbox

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   entry: './src/app.js',
@@ -48,6 +49,7 @@ module.exports = {
     new CopyWebpackPlugin([
       { from: './src/assets', to: 'assets' }
     ]),
+    new Dotenv(),
     new webpack.EnvironmentPlugin({...process.env})
   ]
 }
