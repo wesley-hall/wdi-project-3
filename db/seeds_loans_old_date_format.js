caa8deb9f5359199e9a2155c3df1ea8fd1aad4b4
wesley-hall <hallwesley@outlook.com>
2019-07-08 10:19:07 +0100

    loan seeds old date format backup

---
+++ db/seeds_loans_old_date_format.js
@@ -0,0 +1,183 @@
+[
+  {
+    book: books[0],
+    borrower: users[2],
+    start: '2019-01-15T10:14:31.561Z',
+    end: '2019-02-25T10:14:31.561Z',
+    message: 'Loan A',
+    returned: null,
+    approved: true
+  },
+  {
+    book: books[5],
+    borrower: users[0],
+    start: '2019-01-15T10:14:31.561Z',
+    end: '2019-02-25T10:14:31.561Z',
+    message: 'Loan B',
+    returned: null,
+    approved: true,
+    collected: '2019-01-15T12:15:08.561Z'
+  },
+  {
+    book: books[90],
+    borrower: users[1],
+    start: '2019-01-15T10:14:31.561Z',
+    end: '2019-02-25T10:14:31.561Z',
+    message: 'Loan C',
+    returned: null,
+    approved: true
+  },
+  {
+    book: books[85],
+    borrower: users[4],
+    start: '2019-01-15T10:14:31.561Z',
+    end: '2019-02-25T10:14:31.561Z',
+    message: 'Loan D',
+    returned: null,
+    approved: true
+  },
+  {
+    book: books[2],
+    borrower: users[3],
+    start: '2019-02-19T10:14:31.561Z',
+    end: '2019-02-25T10:14:31.561Z',
+    message: 'Loan E',
+    returned: '2019-02-25T10:14:31.561Z',
+    approved: true
+  },
+  {
+    book: books[86],
+    borrower: users[0],
+    start: '2019-02-19T10:14:31.561Z',
+    end: '2019-02-25T10:14:31.561Z',
+    message: 'Loan F',
+    returned: '2019-02-25T10:14:31.561Z',
+    approved: true
+  },
+  {
+    book: books[75],
+    borrower: users[3],
+    start: '2019-02-19T10:14:31.561Z',
+    end: '2019-02-25T10:14:31.561Z',
+    message: 'Loan G',
+    returned: '2019-02-25T10:14:31.561Z',
+    approved: true
+  },
+  {
+    book: books[76],
+    borrower: users[3],
+    start: '2019-02-20T10:14:31.561Z',
+    end: '2019-02-25T10:14:31.561Z',
+    message: 'Loan H',
+    returned: '2019-02-25T10:14:31.561Z',
+    approved: true
+  },
+  {
+    book: books[87],
+    borrower: users[10],
+    start: '2019-02-20T10:14:31.561Z',
+    end: '2019-02-25T10:14:31.561Z',
+    message: 'Loan I',
+    returned: '2019-02-25T10:14:31.561Z',
+    approved: true
+  },
+  {
+    book: books[3],
+    borrower: users[2],
+    start: '2019-02-20T10:14:31.561Z',
+    end: '2019-02-25T10:14:31.561Z',
+    message: 'Loan J',
+    returned: '2019-02-25T10:14:31.561Z',
+    approved: true
+  },
+  {
+    book: books[6],
+    borrower: users[2],
+    start: '2019-03-20T10:14:31.561Z',
+    end: '2019-04-20T10:14:31.561Z',
+    message: 'Loan K',
+    returned: null,
+    approved: false
+  },
+  {
+    book: books[5],
+    borrower: users[3],
+    start: '2019-03-20T10:14:31.561Z',
+    end: '2019-04-20T10:14:31.561Z',
+    message: 'Loan L',
+    returned: null,
+    approved: true
+  },
+  {
+    book: books[5],
+    borrower: users[1],
+    start: '2019-05-15T10:14:31.561Z',
+    end: '2019-05-25T10:14:31.561Z',
+    message: 'Loan M',
+    returned: null,
+    approved: false
+  },
+  {
+    book: books[89],
+    borrower: users[1],
+    start: '2019-05-15T10:14:31.561Z',
+    end: '2019-05-25T10:14:31.561Z',
+    message: 'Loan N',
+    returned: null,
+    approved: false
+  },
+  {
+    book: books[88],
+    borrower: users[2],
+    start: '2019-05-15T10:14:31.561Z',
+    end: '2019-05-25T10:14:31.561Z',
+    message: 'Loan O',
+    returned: null,
+    approved: false
+  },
+  {
+    book: books[84],
+    borrower: users[1],
+    start: '2019-05-15T10:14:31.561Z',
+    end: '2019-05-25T10:14:31.561Z',
+    message: 'Loan P',
+    returned: null,
+    approved: false
+  },
+  {
+    book: books[83],
+    borrower: users[2],
+    start: '2019-05-15T10:14:31.561Z',
+    end: '2019-05-25T10:14:31.561Z',
+    message: 'Loan Q',
+    returned: null,
+    approved: false
+  },
+  {
+    book: books[78],
+    borrower: users[1],
+    start: '2019-05-15T10:14:31.561Z',
+    end: '2019-05-25T10:14:31.561Z',
+    message: 'Loan R',
+    returned: null,
+    approved: false
+  },
+  {
+    book: books[77],
+    borrower: users[2],
+    start: '2019-05-15T10:14:31.561Z',
+    end: '2019-05-25T10:14:31.561Z',
+    message: 'Loan S',
+    returned: null
+  },
+  {
+    book: books[5],
+    borrower: users[0],
+    start: '2019-05-20T10:14:31.561Z',
+    end: '2019-05-30T10:14:31.561Z',
+    message: 'Loan T',
+    returned: null,
+    approved: false
+  }
+
+]
